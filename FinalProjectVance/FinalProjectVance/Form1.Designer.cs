10ba3f624f8e532ca5ec559880ce61b14594f02b
Matthew Vance <32961154+vipervire@users.noreply.github.com>
2017-10-20 15:06:23 -0500

    Add files via upload

---
+++ FinalProjectVance/FinalProjectVance/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace FinalProjectVance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtEnterUnit = new System.Windows.Forms.TextBox();
+            this.txtEnterName = new System.Windows.Forms.TextBox();
+            this.txtEnterPrice = new System.Windows.Forms.TextBox();
+            this.txtEnterQty = new System.Windows.Forms.TextBox();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQty = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.database2DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database2DataSet = new FinalProjectVance.Database2DataSet();
+            this.database2DataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.database2DataSetBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblEnterID = new System.Windows.Forms.Label();
+            this.lblTotalValue = new System.Windows.Forms.Label();
+            this.txtEnterBrand = new System.Windows.Forms.TextBox();
+            this.lblBrand = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(81, 28);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 46);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(81, 28);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(12, 80);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(81, 28);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtEnterUnit
+            // 
+            this.txtEnterUnit.Location = new System.Drawing.Point(53, 265);
+            this.txtEnterUnit.Name = "txtEnterUnit";
+            this.txtEnterUnit.Size = new System.Drawing.Size(77, 20);
+            this.txtEnterUnit.TabIndex = 3;
+            // 
+            // txtEnterName
+            // 
+            this.txtEnterName.Location = new System.Drawing.Point(53, 151);
+            this.txtEnterName.Name = "txtEnterName";
+            this.txtEnterName.Size = new System.Drawing.Size(77, 20);
+            this.txtEnterName.TabIndex = 0;
+            // 
+            // txtEnterPrice
+            // 
+            this.txtEnterPrice.Location = new System.Drawing.Point(53, 189);
+            this.txtEnterPrice.Name = "txtEnterPrice";
+            this.txtEnterPrice.Size = new System.Drawing.Size(77, 20);
+            this.txtEnterPrice.TabIndex = 1;
+            // 
+            // txtEnterQty
+            // 
+            this.txtEnterQty.Location = new System.Drawing.Point(53, 227);
+            this.txtEnterQty.Name = "txtEnterQty";
+            this.txtEnterQty.Size = new System.Drawing.Size(77, 20);
+            this.txtEnterQty.TabIndex = 2;
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Location = new System.Drawing.Point(12, 268);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(26, 13);
+            this.lblUnit.TabIndex = 8;
+            this.lblUnit.Text = "Unit";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 154);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "Name";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(12, 192);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 10;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblQty
+            // 
+            this.lblQty.AutoSize = true;
+            this.lblQty.Location = new System.Drawing.Point(12, 230);
+            this.lblQty.Name = "lblQty";
+            this.lblQty.Size = new System.Drawing.Size(23, 13);
+            this.lblQty.TabIndex = 11;
+            this.lblQty.Text = "Qty";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(142, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(604, 371);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // database2DataSetBindingSource
+            // 
+            this.database2DataSetBindingSource.DataSource = this.database2DataSet;
+            this.database2DataSetBindingSource.Position = 0;
+            // 
+            // database2DataSet
+            // 
+            this.database2DataSet.DataSetName = "Database2DataSet";
+            this.database2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database2DataSetBindingSource1
+            // 
+            this.database2DataSetBindingSource1.DataSource = this.database2DataSet;
+            this.database2DataSetBindingSource1.Position = 0;
+            // 
+            // database2DataSetBindingSource2
+            // 
+            this.database2DataSetBindingSource2.DataSource = this.database2DataSet;
+            this.database2DataSetBindingSource2.Position = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 326);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(81, 28);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(12, 120);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 13;
+            this.lblID.Text = "ID";
+            // 
+            // lblEnterID
+            // 
+            this.lblEnterID.AutoSize = true;
+            this.lblEnterID.Location = new System.Drawing.Point(50, 120);
+            this.lblEnterID.Name = "lblEnterID";
+            this.lblEnterID.Size = new System.Drawing.Size(0, 13);
+            this.lblEnterID.TabIndex = 14;
+            // 
+            // lblTotalValue
+            // 
+            this.lblTotalValue.AutoSize = true;
+            this.lblTotalValue.Location = new System.Drawing.Point(9, 367);
+            this.lblTotalValue.Name = "lblTotalValue";
+            this.lblTotalValue.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalValue.TabIndex = 15;
+            // 
+            // txtEnterBrand
+            // 
+            this.txtEnterBrand.Location = new System.Drawing.Point(53, 300);
+            this.txtEnterBrand.Name = "txtEnterBrand";
+            this.txtEnterBrand.Size = new System.Drawing.Size(77, 20);
+            this.txtEnterBrand.TabIndex = 4;
+            // 
+            // lblBrand
+            // 
+            this.lblBrand.AutoSize = true;
+            this.lblBrand.Location = new System.Drawing.Point(12, 303);
+            this.lblBrand.Name = "lblBrand";
+            this.lblBrand.Size = new System.Drawing.Size(35, 13);
+            this.lblBrand.TabIndex = 17;
+            this.lblBrand.Text = "Brand";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 392);
+            this.Controls.Add(this.lblBrand);
+            this.Controls.Add(this.txtEnterBrand);
+            this.Controls.Add(this.lblTotalValue);
+            this.Controls.Add(this.lblEnterID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblQty);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.txtEnterQty);
+            this.Controls.Add(this.txtEnterPrice);
+            this.Controls.Add(this.txtEnterName);
+            this.Controls.Add(this.txtEnterUnit);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inventory Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSetBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtEnterUnit;
+        private System.Windows.Forms.TextBox txtEnterName;
+        private System.Windows.Forms.TextBox txtEnterPrice;
+        private System.Windows.Forms.TextBox txtEnterQty;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQty;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource database2DataSetBindingSource;
+        private Database2DataSet database2DataSet;
+        private System.Windows.Forms.BindingSource database2DataSetBindingSource1;
+        private System.Windows.Forms.BindingSource database2DataSetBindingSource2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblEnterID;
+        private System.Windows.Forms.Label lblTotalValue;
+        private System.Windows.Forms.TextBox txtEnterBrand;
+        private System.Windows.Forms.Label lblBrand;
+    }
+}
+
---
+++ FinalProjectVance/FinalProjectVance/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+/*
+ * Name: Matthew Vance
+ * Date: 5/4/2017
+ * Program Name: Inventory Manager
+ * Program Description: A program that can Add, Delete, and Update entries into a database and display them.
+ */
+
+namespace FinalProjectVance
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\1104\Desktop\C#\FinalProjectVance_5-4-17\FinalProjectVance\FinalProjectVance\Database2.mdf;Integrated Security=True");
+
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        //Event Method to add entered info into a record in the database.
+        {
+            if (txtEnterUnit.Text != "" && txtEnterName.Text != "" && txtEnterPrice.Text != "" && txtEnterQty.Text != "")
+            {
+                cmd = new SqlCommand("INSERT INTO [Table] (Unit,Name,Price,Quantity,Brand) values (@Unit,@Name,@Price,@Quantity,@Brand)", cn);
+                cn.Open();
+                cmd.Parameters.AddWithValue("@Unit", txtEnterUnit.Text);
+                cmd.Parameters.AddWithValue("@Name", txtEnterName.Text);
+                cmd.Parameters.AddWithValue("@Price", txtEnterPrice.Text);
+                cmd.Parameters.AddWithValue("@Quantity", txtEnterQty.Text);
+                cmd.Parameters.AddWithValue("@Brand", txtEnterBrand.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Success!\nRecord Added.");
+                UpdateDataGridView();
+                ClearEntryData();
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Details for a Record.");
+            }
+        }
+        private void btnUpdate_Click(object sender, EventArgs e)
+        //Event Method to update entered info into a record in the database.
+        {
+            if (txtEnterUnit.Text != "" && txtEnterName.Text != "" && txtEnterPrice.Text != "" && txtEnterQty.Text != "")
+            {
+                cmd = new SqlCommand("UPDATE [Table] SET Unit=@Unit,Name=@Name,Price=@Price,Quantity=@Quantity,Brand=@Brand WHERE ID=@ID", cn);
+                cn.Open();
+                cmd.Parameters.AddWithValue("@ID", lblEnterID.Text);
+                cmd.Parameters.AddWithValue("@Unit", txtEnterUnit.Text);
+                cmd.Parameters.AddWithValue("@Name", txtEnterName.Text);
+                cmd.Parameters.AddWithValue("@Price", txtEnterPrice.Text);
+                cmd.Parameters.AddWithValue("@Quantity", txtEnterQty.Text);
+                cmd.Parameters.AddWithValue("@Brand", txtEnterBrand.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Success!\nRecord Updated.");
+                cn.Close();
+                UpdateDataGridView();
+                ClearEntryData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Record to Update.");
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        //Event Method to delete selected info from a record in the database.
+        {
+            if (lblEnterID.Text != "")
+            {
+                cmd = new SqlCommand("DELETE [Table] WHERE ID=@ID", cn);
+                cn.Open();
+                cmd.Parameters.AddWithValue("@ID", lblEnterID.Text);
+                cmd.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("Success!\nRecord Deleted.");
+                UpdateDataGridView();
+                ClearEntryData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select a Record to Delete.");
+            }
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        //Event Method to assign values to textboxes and labels from datagridview upon clicking row header.
+        {
+            lblEnterID.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtEnterName.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtEnterPrice.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtEnterQty.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtEnterUnit.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtEnterBrand.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        //Event Method to exit program.
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        //Event Method upon load to display info in datagridview.
+        {
+            UpdateDataGridView();
+        }
+        private void UpdateDataGridView()
+        //Method to display database info into datagridview and create the total value of inventory.
+        {
+            cn.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("SELECT * FROM [Table] ORDER BY ID", cn);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cn.Close();
+            //foreach record in dataset, Price * Quantity and accumulate.
+            double totalValue = 0.0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                double priceInventory, quantityInventory;
+
+                if (!double.TryParse(dr["Price"].ToString(), out priceInventory))
+                {
+                    break;
+                }
+                if (!double.TryParse(dr["Quantity"].ToString(), out quantityInventory))
+                {
+                    break;
+                }
+                totalValue += priceInventory * quantityInventory;
+            }
+            string stringTotalValue = String.Format("{0:C}", totalValue);
+            lblTotalValue.Text = "Total Value: " + stringTotalValue;
+        }
+        private void ClearEntryData()
+        //Method to clear info from textboxes and label.
+        {
+            txtEnterUnit.Text = "";
+            txtEnterName.Text = "";
+            txtEnterPrice.Text = "";
+            txtEnterQty.Text = "";
+            lblEnterID.Text = "";
+            txtEnterBrand.Text = "";
+        }
+    }
+}
